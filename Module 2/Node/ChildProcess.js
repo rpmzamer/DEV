75e76d3d923ad617c42f19613f94011fbf61e488
rpmzamer <mayankaryarpm@gmail.com>
2022-03-10 21:51:11 +0530

    Test.js added

--- Module 2/Node/ChildProcess.js
+++ Module 2/Node/ChildProcess.js
@@ -1,5 +1,12 @@
 let chd = require("child_process"); //user to acquire package
-console.log(chd);
+//console.log(chd);
 
 //for opening calculator (for ubuntu "gnome-calculator" )
-chd.execFileSync("calc");
+//chd.execFileSync("calc");
+
+//open vscode with childprocess
+//chd.execSync("code");
+
+//open a js file
+let output = chd.execSync("node Test.js");
+console.log(''+output);
