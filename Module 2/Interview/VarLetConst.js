fa2c5d425ab76ff80c79f5c588b9f6f116287171
rpmzamer <mayankaryarpm@gmail.com>
2022-03-24 23:27:37 +0530

    VarLetConst

---
+++ Module 2/Interview/VarLetConst.js
@@ -0,0 +1,31 @@
+//hoisting
+console.log(a);
+var a = 10;
+
+console.log(a);
+var a;
+a = 10;
+console.log(a);
+
+a = 20;
+
+console.log(a);
+
+
+var a = 50;
+console.log(a);
+
+// console.log(l);      //Error
+let l ;              
+l = 10;   
+console.log(l);
+// let l =7;            //Error
+
+
+//const
+// cont c;   
+// console.log(c);      //Error
+const c = 30;
+console.log(c);
+// const c=10;         //Error  
+
