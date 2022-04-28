971d923a3c8fbbf112d94a4de7a38a00638b1b24
rpmzamer <mayankaryarpm@gmail.com>
2022-04-28 20:14:34 +0530

    Github Scrapper Started

---
+++ Module 2/Web Scrapping/githubScrapper.js
@@ -0,0 +1,26 @@
+const request = require("request");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+
+const link = "https://github.com/topics";
+
+request(link,cb);
+
+
+function cb(error,response,html){
+    if(error){
+        console.log(error);
+    }
+    else
+    {
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let allAnchorTags = document.querySelectorAll(".no-underline.d-flex.flex-column.flex-justify-center");
+        for(let i=0;i<allAnchorTags.length;i++)
+        {
+            let CompleteLink="https://github.com"+allAnchorTags[i].href;
+            console.log(CompleteLink);
+        }
+    }
+}    
