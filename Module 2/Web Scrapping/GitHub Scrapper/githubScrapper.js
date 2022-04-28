c6aef1f9bd2c486e11b0a6bb988f07798daec957
rpmzamer <mayankaryarpm@gmail.com>
2022-04-29 02:40:22 +0530

    gitHubScrapper (half done, directory work remaining)

--- Module 2/Web Scrapping/GitHub Scrapper/githubScrapper.js
+++ Module 2/Web Scrapping/GitHub Scrapper/githubScrapper.js
@@ -1,6 +1,7 @@
 const request = require("request");
 const jsdom = require("jsdom");
 const fs = require("fs");
+const path = require("path");
 const { JSDOM } = jsdom;
 
 const link = "https://github.com/topics";
@@ -19,13 +20,15 @@ function cb(error,response,html){
         const document = dom.window.document;
         let allAnchorTags = document.querySelectorAll(".no-underline.d-flex.flex-column.flex-justify-center");
         let textTags= document.querySelectorAll(".f3.lh-condensed.text-center.Link--primary.mb-0.mt-1");
-        for(let i=0;i<allAnchorTags.length;i++)
+        for(let i=0;i<1;i++)         //allAnchorTags.length
         {
             let CompleteLink="https://github.com"+allAnchorTags[i].href;
-            parentfolderName = textTags[i].textContent;
-        
-            if(!fs.existsSync(parentfolderName))
-                fs.mkdirSync(parentfolderName);
+            let parentfolderName = textTags[i].textContent.trim();           // We used Trim() because the text had whitespaces
+            // console.log(parentfolderName);
+            // if(!fs.existsSync(parentfolderName))
+            // {
+            //     fs.mkdirSync(parentfolderName);
+            // }    
             // console.log(CompleteLink);
             request(CompleteLink,cb2);
         }
@@ -40,10 +43,35 @@ function cb2(error,response,html)
     }
     else
     {
-        allGitRepoName = document.querySelectorAll(".f3.color-fg-muted.text-normal.lh-condensed");
-        for(let i=0;i<8;i++)
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let allGitRepoName = document.querySelectorAll(".f3.color-fg-muted.text-normal.lh-condensed");
+        for(let i=0;i<1;i++)
         {
-            console.log(allGitRepoName[i].textContent);
+            let s=allGitRepoName[i].textContent.split("/");    // since repoName was comming for e.g        wordplate  /       wordplate
+            let singleRepoName=s[0].trim()+"/"+s[1].trim();    // We used Trim() because the text had whitespaces
+            let completeRepoIssueLink = "https://github.com/"+singleRepoName+"/issues";
+            // console.log(completeRepoIssueLink);
+            request(completeRepoIssueLink,cb3);       
         }
+    }
+}
+
+function cb3(error,response,html)
+{
+    if(error)
+    {
+        console.log("Error Found: ");
+    }
+    else
+    {
+        const dom = new JSDOM(html);
+        const document = dom.window.document;
+        let allIssuesTags = document.querySelectorAll(".d-block.d-md-none.position-absolute.top-0.bottom-0.left-0.right-0");
+        for(let i=0;i<1;i++)
+        {
+            console.log(allIssuesTags[i].textContent); //to be resolved
+            console.log("https://github.com"+allIssuesTags[i].href);
+        }      
     }
 }
