00a1c82e780975de63034e64790188c97d95692b
rpmzamer <mayankaryarpm@gmail.com>
2022-05-08 23:57:56 +0530

    debugging

--- Module 2/Web Scrapping/GitHub Scrapper/githubScrapper.js
+++ Module 2/Web Scrapping/GitHub Scrapper/githubScrapper.js
@@ -3,11 +3,13 @@ const jsdom = require("jsdom");
 const fs = require("fs");
 const path = require("path");
 const { JSDOM } = jsdom;
-
 let issues=[];
 const link = "https://github.com/topics";
 let counter = 0 ;
-let repoFolderPath ="";
+let topicpath = [];
+let repoFolderPath = [];
+let j=0;
+let k=0;
 request(link,cb);
 
 
@@ -24,18 +26,18 @@ function cb(error,response,html){
         let textTags= document.querySelectorAll(".f3.lh-condensed.text-center.Link--primary.mb-0.mt-1");
         for(let i=0;i<allAnchorTags.length;i++)         //allAnchorTags.length
         {
-            repoFolderPath ="";
+            
             let CompleteLink="https://github.com"+allAnchorTags[i].href;
             let parentfolderName = textTags[i].textContent.trim();           // We used Trim() because the text had whitespaces
             // console.log(parentfolderName);
             if(!fs.existsSync(parentfolderName))
             {
                 fs.mkdirSync(parentfolderName);
-                repoFolderPath = path.join(__dirname,parentfolderName);
+                topicpath[i] = path.join(__dirname,parentfolderName);
             }    
             // console.log(CompleteLink);
             request(CompleteLink,cb2);
-            counter++;
+            
         }
     }
 }    
@@ -51,19 +53,28 @@ function cb2(error,response,html)
         const dom = new JSDOM(html);
         const document = dom.window.document;
         let allGitRepoName = document.querySelectorAll(".f3.color-fg-muted.text-normal.lh-condensed");
-        for(let i=0;i<1;i++)
+        let termcb2 = 8;
+        if(allGitRepoName.length<8)
+        termcb2=allGitRepoName.length;
+        for(let i=0;i<termcb2;i++)
         {
             let s=allGitRepoName[i].textContent.split("/");    // since repoName was comming for e.g        wordplate  /       wordplate
-            let singleRepoName=s[0].trim()+"/"+s[1].trim();    // We used Trim() because the text had whitespaces
+            let singleRepoName = s[0].trim()+"/"+s[1].trim();    // We used Trim() because the text had whitespaces         
+            let singleRepoNameCrop = s[0].trim();
             let completeRepoIssueLink = "https://github.com/"+singleRepoName+"/issues";
             // console.log(completeRepoIssueLink);
-             if(!fs.existsSync(singleRepoName))
+            // console.log(topicpath[j]+"\\" + singleRepoNameCrop);
+                                                                                     //path.join(repoFolderPath),singleRepoName)
+             if(!fs.existsSync(topicpath[j]+"\\" + singleRepoNameCrop))
              {
-                fs.mkdirSync(singleRepoName);
-                repoFolderPath = path.join(repoFolderPath,singleRepoName);
-             }   
-            request(completeRepoIssueLink,cb3);       
+                fs.mkdirSync(topicpath[j]+"\\" + singleRepoNameCrop);
+                repoFolderPath.push(topicpath[j]+"\\" + singleRepoNameCrop);
+             }
+            //  console.log(completeRepoIssueLink);
+             request(completeRepoIssueLink,cb3); 
+             counter++;      
         }
+        j++;
     }
 }
 
@@ -71,27 +82,35 @@ function cb3(error,response,html)
 {
     if(error)
     {
-        console.log("Error Found: ");
+        console.log("Error Found: "+error);
     }
     else
     {
         const dom = new JSDOM(html);
         const document = dom.window.document;
         let allIssuesTags = document.querySelectorAll(".Link--primary.v-align-middle.no-underline.h4.js-navigation-open.markdown-title");
-        for(let i=0;i<8;i++)
+        let term = 8;
+        if(allIssuesTags.length<8)
+        term=allIssuesTags.length;
+        for(let i=0;i<term;i++)
         {
+            // console.log(allIssuesTags[i].textContent);
             let issName = allIssuesTags[i].textContent;
             let isslink = "https://github.com"+allIssuesTags[i].href;
             issuesProcess(issName,isslink);
         }  
-        
+        // let data = JSON.stringify(issues);
+        console.log(repoFolderPath.length);
+        // fs.writeFileSync(path.join(repoFolderPath[k],issues.json),data);
+        issues=[];
         counter--;
-        if(counter==0)
-        {
-            console.log(issues);
-            let data = JSON.stringify(issues);                // because writeFileSync reads only string so for converting object to string we use JSON
-            fs.writeFileSync(path.join(repoFolderPath,Issues.json),data);
-        }
+        // if(counter==0)
+        // {
+        //     console.log(issues);
+        //     let data = JSON.stringify(issues);                       // because writeFileSync reads only string so for converting object to string we use JSON
+        //     fs.writeFileSync(path.join(repoFolderPath[k],Issues.json),data);
+        // }
+        k++;
     }
 }
 
